1de9985da81fe712c0053e54bbf02f0a58aba283
minia <minia@DESKTOP-R6I925E>
2016-11-22 12:02:12 -0700

    I did something, I swear

---
+++ Type Quest/src/typeQuest/Item.java
@@ -0,0 +1,13 @@
+package typeQuest;
+
+public class Item {
+	String type;
+	String description;
+	int weight;
+	
+	Item(String type, int weight, String description) {
+		this.type = type;
+		this.description = description;
+		this.weight = weight;
+	}
+}
--- Type Quest/src/typeQuest/Room.java
+++ Type Quest/src/typeQuest/Room.java
@@ -1,5 +1,7 @@
 package typeQuest;
 
 public class Room {
-
+	String des = "";
+	Map[] room = new Map[33];
+	Map[] hidden = new Map[8];
 }
